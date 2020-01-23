5dc583a2aa5a4e8306e9f2a4318f8a1ee2976225
Grant Simmons <gsimm706@gmail.com>
2020-01-23 18:07:03 -0500

    Change window title to something more appropriate

--- source/blit.cpp
+++ source/blit.cpp
@@ -274,7 +274,7 @@ bool init(){
         //srand(time(NULL));
 
 		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("GameBuddy GameBoy Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if(gWindow == NULL){
 			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
